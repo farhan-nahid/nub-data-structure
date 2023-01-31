4196b735dd0c57d4db0cb04662ee4293969c0a15
farhan-nahid <nahidahmed9933@gmail.com>
2023-01-31 09:22:57 +0600

    task3 solved using pointer

--- assignments/assignment1/task3/main.cpp
+++ assignments/assignment1/task3/main.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-void delete_value(int arr[], int size, int index){  
+int* delete_value(int arr[], int size, int index){  
     for(int i = index + 1; i <= size; i++){
          arr[i - 1] = arr[i];
     }
     
-    for(int i = 0; i < size; i++){
-         cout << arr[i] << endl;
-    }
+    return arr;
 }
 
 
 int main(){
     int arr[] = {1,2,3,4}, size = 4, index = 2;
 
-    delete_value(arr, size, index);
+    int* result = delete_value(arr, size, index);
+
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << endl;
+    }
 
     return 0;
 }
