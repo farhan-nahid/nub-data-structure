7063a36530d9efa4c39fe81ff6104bf74db1479d
Farhan Ahmed Nahid <nahidahmed9933@gmail.com>
2023-02-14 17:38:55 +0600

    solved

--- assignments/assignment2/task8/main.cpp
+++ assignments/assignment2/task8/main.cpp
@@ -3,39 +3,67 @@
 
 using namespace std;
 
-
-
-void choose_even_number (int arr1[], int arr2[]) {
-    int result[10] = {}, position = 0 , i = 0, j = 0, k = 0;
-   
-
-    while (arr1[i]) {
-        if(arr1[i] % 2 == 0){
-            result[position] = arr1[i];
-            position ++;
+void for_loop(int arr[], int size, int count){
+     for (int i = 0; i < size; i++) {
+        if(arr[i] % 2 == 0) {
+            count++;
+            // return arr[i];
+            // totalEvenNumberArray[totalArrayIndexCount] = arr[i];
         }
-      i++;
     }
-    
-    while (arr2[j]) {
-        if(arr2[j] % 2 == 0){
-            result[position] = arr2[j];
-            position ++;
-        }
-        j++;
-    }
-
-    while(result[k]){
-        cout << result[k] << endl;
-        k++;
-    }
-
 }
+
+
+// this function counts the number of even numbers in both array
+int getEvenNumberCount(int arr1[], int arr2[], int size1, int size2) {
+    int evenNumberCount = 0;
+    for_loop(arr1, size1, evenNumberCount);
+    for_loop(arr2, size2, evenNumberCount);
+    for (int i = 0; i < size1; i++) {
+        if(arr1[i] % 2 == 0) {
+            evenNumberCount++;
+        }
+    }
+    for (int i = 0; i < size2; i++) {
+        if(arr2[i] % 2 == 0) {
+            evenNumberCount++;
+        }
+    }
+
+    cout << evenNumberCount;
+    return evenNumberCount;
+}
+
+
+void choose_even_number (int arr1[], int arr2[], int size1, int size2) {
+    // get count
+    int evenNumberCount = getEvenNumberCount(arr1, arr2, size1, size2);
+    // declare new array
+    int totalEvenNumberArray[evenNumberCount];
+    int totalArrayIndexCount = 0;
+    for (int i = 0; i < size1; i++) {
+        if(arr1[i] % 2 == 0) {
+            totalEvenNumberArray[totalArrayIndexCount] = arr1[i];
+            totalArrayIndexCount++;
+        }
+    }
+    for (int i = 0; i < size2; i++) {
+        if(arr2[i] % 2 == 0) {
+            totalEvenNumberArray[totalArrayIndexCount] = arr2[i];
+            totalArrayIndexCount++;
+        }
+    }
+    for (int i = 0; i < totalArrayIndexCount; i++) {
+        cout << totalEvenNumberArray[i] <<endl;
+    }
+}
+
+
 
 int main(){
     int arr1[3] = {1,2,3}, arr2[3] = {4,5,6};
 
-    choose_even_number(arr1, arr2); 
+    choose_even_number(arr1, arr2, sizeof(arr1) / sizeof(arr1[0]), sizeof(arr2) / sizeof(arr2[0])); 
 
     return 0;
 }
